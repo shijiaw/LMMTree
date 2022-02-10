79f752e827bb9b2b6859d5f56f3107721151115b
Shijia Wang <shijia_wang@nankai.edu.cn>
2022-02-10 13:41:33 +0800

    Add files via upload

---
+++ Simulation/S5/2/mb.R
@@ -0,0 +1,63 @@
+library(stringr)
+library("phyclust")
+#library(phylotate)
+
+mbtree_Dir <- 'mkdir mb_tree'
+if (!file.exists('mb_tree')){
+  system('mkdir mb_tree')
+}
+
+nuncertanty <- 100
+M = 15
+mysub <- function(test){
+  test.new <- test
+  index1 <- unlist(gregexpr(pattern="\\[", test,perl = TRUE)) 
+  index2 <- unlist(gregexpr(pattern="\\]", test,perl = TRUE)) 
+  substr(test,index1[1], index2[1])
+  M <- substr(test,1,(index2[1]+1))
+  test.new <- str_replace(test.new,fixed(M),"")
+  #test.new <- str_replace(test.new,";" ,"")
+  # for(i in 2:length(index1)){
+  #   M <- substr(test, index1[i],index2[i])
+  #   test.new <- str_replace(test.new,fixed(M),"")
+  # }
+  return(list(tree=test.new))
+}
+
+
+#generate mb files and run mb
+for(n in 1:N){
+  file <- paste("output_seq/seq", n, ".nex", sep = '')
+  batchname <- paste("output_seq/batch", n, ".nex", sep = '')
+  cat("begin mrbayes;",file=batchname,sep="\n")
+  cat("set autoclose=yes nowarn=yes;",file=batchname, sep="\n", append=TRUE)
+  cat(paste("execute", file,";"),file=batchname, sep="\n",append=TRUE)
+  cat("prset brlenspr=clock:uniform;",file=batchname, sep="\n",append=TRUE)
+  cat("mcmc nruns=1 nchains=1 ngen=500000 samplefreq=1000;",file=batchname, sep="\n",append=TRUE)
+  cat("sumt burnin = 300000;",file=batchname, sep="\n",append=TRUE)
+  cat("end;",file=batchname,append=TRUE)
+  command_mb = paste("mb output_seq/batch", n, ".nex > log.txt", sep = '')
+  system(command_mb)
+  
+  
+  skipLines = (M + 5)
+  tree_name = paste("output_seq/seq", n, ".nex.t", sep = '')
+  tree.anc <- read.csv(tree_name, skip = skipLines, sep="\t", header = FALSE)
+  ntree <- nrow(tree.anc)
+  thinned_index <- (ntree-nuncertanty):(ntree-1)
+  tree_name2 = paste("output_seq/seq", n, ".nex.con.tre", sep = '')
+  tips <- read.csv(tree_name2, skip = 5, sep="\t", header = FALSE)[1:M,3]
+  index <- 1
+  for(i in thinned_index){
+    test <- mysub(tree.anc[i,])
+    mbtree_name = paste("mb_tree/tree", n, "_", index,".newick", sep = '')
+    index <- index + 1
+    mytree <- read.tree(text = test$tree)
+    mytree$tip.label <- tips[as.numeric(mytree$tip.label)]
+    write.tree(mytree, file = mbtree_name, digits = 12)
+  }
+  
+
+}
+
+
---
+++ Simulation/S5/2/phesim.R
@@ -0,0 +1,45 @@
+rm(list=ls())
+# simluate phenotype
+library("phyclust")
+library("MASS")
+
+N = 50
+M = 15
+L = 100
+beta = 0.1
+sigma_e = 0.1
+sigma_b = 0.2
+
+set.seed(321)
+
+phe_Dir <- 'mkdir output_phenotype'
+if (!file.exists('output_phenotype')){
+  system('mkdir output_phenotype')
+}
+
+index <- rep(NA, N)
+for(i in 1:N){
+  readseq_dir <- paste('output_SNP/SNP',i ,'.csv', sep = '')
+  seq_i <- read.csv(readseq_dir, sep = ' ', header = FALSE)
+  
+  mean_mat <- apply(seq_i, 2, mean)
+  sd_mat <- sqrt(mean_mat*(1-mean_mat))
+  std_mat_i <- apply(seq_i, 2, scale)
+  
+  index[i] <- sample.int(ncol(seq_i), size = 1)
+  
+  readK_dir <- paste('output_tree/K_',i ,'.csv', sep = '')
+  K_tree <- read.csv(readK_dir, sep = ",", header = FALSE)
+  
+  b <- mvrnorm(n = 1, mu = rep(0, M), Sigma = K_tree)*sigma_b
+  eps <- rnorm(M, mean = 0, sd = sigma_e)
+  
+  y = std_mat_i[,index[i]]*beta + b + eps
+  
+  filename = paste('output_phenotype/y_', i, '.csv', sep = "")
+  write.table(y, file = filename, row.names = FALSE, col.names = FALSE)
+}
+write.table(index, file = 'index.csv', row.names = FALSE, col.names = FALSE)
+
+
+
